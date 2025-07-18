1bf32fabc7d24700dec1adfbfb894f73a3ba3b31
loyaltysports <samuelnarino00@gmail.com>
2025-07-18 18:45:07 +0100

    Deleted app-build-manifest.json

--- .next/static/chunks/src_app_activities_page_tsx_a993527f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_140d1a5b._.js",
-  "static/chunks/node_modules_58fd0e07._.js"
-],
-    source: "dynamic"
-});
