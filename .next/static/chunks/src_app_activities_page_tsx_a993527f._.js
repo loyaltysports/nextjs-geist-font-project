1a020fafffe0f8742d4ba14c3f742a98bd6c5dfa
loyaltysports <samuelnarino00@gmail.com>
2025-07-18 13:18:25 +0100

    Add .next/static/chunks/src_app_activities_page_tsx_a993527f._.js 1752841104511

---
+++ .next/static/chunks/src_app_activities_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_140d1a5b._.js",
+  "static/chunks/node_modules_58fd0e07._.js"
+],
+    source: "dynamic"
+});
